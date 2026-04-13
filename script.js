dcf5de2750cb9a41ed61b46778c5f8e692681e06
Alexander Stach <stach.alex@yahoo.de>
2026-04-13 21:25:43 +0200

    Weitere Korrektur für GitHub Pages

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+let slideIndex = 0;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+
+  slides[n].classList.add("active");
+  dots[n].classList.add("active");
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlides(slideIndex);
+}
+
+function currentSlide(n) {
+  slideIndex = n - 1;
+  showSlides(slideIndex);
+}
+
+showSlides(slideIndex);
+setInterval(nextSlide, 4000);
